a0d23735633cf505025c0bcfa881c9fa8531d88e
yosua161@gmail.com <yosua161@gmail.com@c77fe80a-b040-53a3-3fe7-14592e7e8651>
2011-10-31 19:14:29 +0000

    Membuat UC mengatur profil pribadi dengan membuat controller get atur akun dan set atur akun. membuat JSP pengaturan. dan menambahkan dispatcher dengan page "home", "pengaturanAkun" dan "setPengaturanAkun.

--- Smash/src/java/model/Mahasiswa.java
+++ Smash/src/java/model/Mahasiswa.java
@@ -39,6 +39,153 @@ public class Mahasiswa implements Serializable {
     @Column(name = "loginStat")
     private Boolean loginStat;
 
+    @Column(name = "noHp")
+    private String noHp;
+
+    @Column(name = "alamatAsal")
+    private String alamatAsal;
+
+    @Column(name = "alamatSurabaya")
+    private String alamatSurabaya;
+
+    @Column(name = "nilaiToefl")
+    private long nilaiToefl;
+
+    @Column(name = "semester")
+    private long semester;
+
+    @Column(name = "ipk")
+    private long ipk;
+
+    @Column(name = "namaAyah")
+    private String namaAyah;
+
+    @Column(name = "pekerjaanAyah")
+    private String pekerjaanAyah;
+
+    @Column(name = "penghasilanAyah")
+    private long penghasilanAyah;
+
+    @Column(name = "namaIbu")
+    private String namaIbu;
+
+    @Column(name = "pekerjaanIbu")
+    private String pekerjaanIbu;
+
+    @Column(name = "penghasilanIbu")
+    private long penghasilanIbu;
+
+     @Column(name = "jumlahSaudara")
+    private long jumlahSaudara;
+
+    public void setAlamatAsal(String alamatAsal) {
+        this.alamatAsal = alamatAsal;
+    }
+
+    public void setAlamatSurabaya(String alamatSurabaya) {
+        this.alamatSurabaya = alamatSurabaya;
+    }
+
+    public void setIpk(long ipk) {
+        this.ipk = ipk;
+    }
+
+    public void setJumlahSaudara(long jumlahSaudara) {
+        this.jumlahSaudara = jumlahSaudara;
+    }
+
+    public void setNamaAyah(String namaAyah) {
+        this.namaAyah = namaAyah;
+    }
+
+    public void setNamaIbu(String namaIbu) {
+        this.namaIbu = namaIbu;
+    }
+
+    public void setNilaiToefl(long nilaiToefl) {
+        this.nilaiToefl = nilaiToefl;
+    }
+
+    public void setNoHp(String noHp) {
+        this.noHp = noHp;
+    }
+
+    public void setPekerjaanAyah(String pekerjaanAyah) {
+        this.pekerjaanAyah = pekerjaanAyah;
+    }
+
+    public void setPekerjaanIbu(String pekerjaanIbu) {
+        this.pekerjaanIbu = pekerjaanIbu;
+    }
+
+    public void setPenghasilanAyah(long penghasilanAyah) {
+        this.penghasilanAyah = penghasilanAyah;
+    }
+
+    public void setPenghasilanIbu(long penghasilanIbu) {
+        this.penghasilanIbu = penghasilanIbu;
+    }
+
+    public void setSemester(long semester) {
+        this.semester = semester;
+    }
+
+    public String getAlamatAsal() {
+        return alamatAsal;
+    }
+
+    public String getAlamatSurabaya() {
+        return alamatSurabaya;
+    }
+
+    public long getIpk() {
+        return ipk;
+    }
+
+    public long getJumlahSaudara() {
+        return jumlahSaudara;
+    }
+
+    public String getNamaAyah() {
+        return namaAyah;
+    }
+
+    public String getNamaIbu() {
+        return namaIbu;
+    }
+
+    public long getNilaiToefl() {
+        return nilaiToefl;
+    }
+
+    public String getNoHp() {
+        return noHp;
+    }
+
+    public String getPekerjaanAyah() {
+        return pekerjaanAyah;
+    }
+
+    public String getPekerjaanIbu() {
+        return pekerjaanIbu;
+    }
+
+    public long getPenghasilanAyah() {
+        return penghasilanAyah;
+    }
+
+    public long getPenghasilanIbu() {
+        return penghasilanIbu;
+    }
+
+    public long getSemester() {
+        return semester;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
     public void setLoginStat(Boolean loginstat) {
         this.loginStat = loginstat;
     }
